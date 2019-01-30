6c74b604ce8826b0f160a1b04002da85db42c3d0
alifaaz <alifaazali@gmail.com>
2019-01-30 15:21:10 +0300

    add model to tips branche

---
+++ src/features/courses/courses.model.js
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose';
+
+/**
+ * {CoursesSchema}
+ * @param {String} title - course title
+ * @param {String} type - courses type
+ * @param {String} note
+ * @param {String} place - where the course held
+ * @param {Object} Date - conssit of 2 property
+ * 	@param {Date} from
+ * 	@param {Date} to
+ * @param {BOOLEAN} status -  is the trip is finished or not
+ * @param {ENUMstring} interaction - [جيد , متوسط , سيء]
+ * @param {ARRAY} individuals - ids of people who will partcipate in this course
+ * @param {ArrayOfObject}  research
+ * 	@param {STRING} title
+ *@param {ARRAY}  by
+ @param {String} editBy
+
+ *  */
+const courseSchema = new Schema({
+  title: { $type: String, required: true },
+  type: String,
+  note: String,
+  place: String,
+  date: {
+    from: Date,
+    to: Date,
+  },
+  status: String,
+  interaction: { $type: String },
+  research: [{ title: String }],
+  individuals: { $type: Schema.Types.ObjectId, ref: 'Individuals' },
+  by: { $type: Schema.Types.ObjectId, ref: 'Individuals' },
+  editBy: { $type: Schema.Types.ObjectId, ref: 'User' },
+
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  typeKey: '$type',
+  collation: { locale: 'ar' },
+  strictQuery: true,
+});
+
+export default model('Courses', courseSchema);
