e607dc13f544d709bc41d71d9ce0effaac21b814
alifaaz <alifaazali@gmail.com>
2019-02-02 05:45:42 +0300

    dealyed task

--- src/features/index.js
+++ src/features/index.js
@@ -1,11 +1,13 @@
 import individuals from './individuals/individuals.router';
 import trips from './trips/trips.router';
 import courses from './courses/course.router';
-import relations from './relations/relation.router'
-;
+import relations from './relations/relation.router';
+import tasks from './tasks/task.router';
+
 export default {
   individuals,
   trips,
   courses,
   relations,
+  tasks,
 };
---
+++ src/features/tasks/task.model.js
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose'
+
+/**
+ * @param {String} type
+ * @param {String} Dept
+ * @param {ENMU} status
+ *
+ */
+const taskSchem = new Schema({
+
+})
