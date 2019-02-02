90f1d4bf792af174adc63f2e3fa728942daee334
alifaaz <alifaazali@gmail.com>
2019-02-02 20:02:54 +0300

    user moder is ended

---
+++ src/features/users/user.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  pwd: { type: String, required: true },
+  permissions: [String],
+  role: String,
+
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+
+});
+
+
+userSchema.pre('save', (next) => {
+
+	if(!this.isModified('pwd')) return next();
+
+	this.pwd = bcrypt.hashSync(this.pwd, bycrpt.genSaltSync(10));
+
+  return next();
+});
+
+
+export default model('User', userSchema)
